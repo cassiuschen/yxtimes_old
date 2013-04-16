099c8936cace3edebb40d62d457015aa743a2eb5
鲜染 thor <xranthoar@gmail.com>
2013-04-17 00:52:34 +0800

    又合完一个

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,5 +2,17 @@ class Notification
   include Mongoid::Document
   field :content, type: String
 
+  field :read?, type: Boolean, default: false
+
+  field :link, type: String
+
+  def read
+    update_attributes(read?: true)
+  end
+
+  default_scope where(read?: false)
+
+  scope :readed, where(read?: true)
+
   embedded_in :user
 end
