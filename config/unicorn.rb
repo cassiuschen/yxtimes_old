ca6a01c5470fa0898586c0e03dfb8a73c1453b22
鲜染 thor <xranthoar@gmail.com>
2013-04-19 20:01:29 +0800

    .

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,8 +1,8 @@
 working_directory "/home/seiue/CODE/yxtimes/"
 pid "/home/seiue/CODE/yxtimes/tmp/pids/unicorn.pid"
-stderr_path "/home/seiue/CODE/yxtimes//log/unicorn.log"
+stderr_path "/home/seiue/CODE/yxtimes/log/unicorn.log"
 stdout_path "/home/seiue/CODE/yxtimes/log/unicorn.log"
 
 listen "/tmp/unicorn.yxtimes.sock"
-worker_processes 2
+worker_processes 4
 timeout 30
