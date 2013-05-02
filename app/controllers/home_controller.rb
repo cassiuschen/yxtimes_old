1e25a5cb519cc43c8617afe4ddcbea310d95dbcf
bdfz <bdfz@yxtimes.(none)>
2013-05-02 10:00:41 +0800

    modified:   app/controllers/home_controller.rb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,16 +1,15 @@
 class HomeController < ApplicationController
   def index
     @feature = Feature.first
-    if @feature == nil
-      redirect_to new_feature_path, notice: "请至少创建一个专题。"
-      return
-    end
     @diaocha = Category.diaocha.articles.with_img.hottest.first || Category.diaocha.articles.hottest.first
     @guandian = Category.guandian.articles.hottest
     @renwu = Category.renwu.articles.with_img.hottest.first || Category.renwu.articles.hottest.first
     @yuedu = Category.yuedu.articles.hottest
     @vote = Vote.first
     @most_read = Article.hottest
+    unless @feature && @diaocha && @guandian && @renwu && @yuedu && @vote 
+      redirect_to new_article_path, notice: "请至少每个目录创建一篇文章，否则首页无法显示"
+    end
   end
 
   def about
