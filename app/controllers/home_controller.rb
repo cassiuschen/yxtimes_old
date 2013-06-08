98c10fccaec2cc41f29cd7b61a6edb828c8fcb23
bdfz <bdfz@yxtimes.org>
2013-06-08 13:34:45 +0800

    change hottest to recent

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,10 @@
 class HomeController < ApplicationController
   def index
     @feature = Feature.first
-    @diaocha = Category.diaocha.articles.with_img.hottest.first || Category.diaocha.articles.hottest.first
-    @guandian = Category.guandian.articles.hottest
-    @renwu = Category.renwu.articles.with_img.hottest.first || Category.renwu.articles.hottest.first
-    @yuedu = Category.yuedu.articles.hottest
+    @diaocha = Category.diaocha.articles.with_img.recent.first || Category.diaocha.articles.hottest.first
+    @guandian = Category.guandian.articles.recent
+    @renwu = Category.renwu.articles.with_img.recent.first || Category.renwu.articles.hottest.first
+    @yuedu = Category.yuedu.articles.recent
     @vote = Vote.first
     @most_read = Article.hottest
   end
